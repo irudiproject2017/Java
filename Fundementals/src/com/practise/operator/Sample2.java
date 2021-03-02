308f1554cd5bb7481ce35a0dd28a891b012d8e1b
iruthayaswamy <irudiproject2017@gmail.com>
2021-03-02 15:56:31 +0530

    Decrement operator test

--- Fundementals/src/com/practise/operator/Sample2.java
+++ Fundementals/src/com/practise/operator/Sample2.java
@@ -3,7 +3,6 @@ package com.practise.operator;
 public class Sample2 {
     public static void main(String[] args) {
         int a=10;
-        a++;
-        System.out.println("Testing operator"+ a);
+        System.out.println("Testing operator"+ (--a));
     }
 }
