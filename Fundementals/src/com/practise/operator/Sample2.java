36b4a59f1c2fa7882ba0465842332614deb4c928
iruthayaswamy <irudiproject2017@gmail.com>
2021-03-02 15:51:35 +0530

    Increment operator test

--- Fundementals/src/com/practise/operator/Sample2.java
+++ Fundementals/src/com/practise/operator/Sample2.java
@@ -2,6 +2,8 @@ package com.practise.operator;
 
 public class Sample2 {
     public static void main(String[] args) {
-        System.out.println("Testing operator");
+        int a=10;
+        a++;
+        System.out.println("Testing operator"+ a);
     }
 }
